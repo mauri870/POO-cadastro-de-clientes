944debeeb1040c44c73a9b21f74599801eebff0d
Mauri de Souza Nunes <mauri870@gmail.com>
2015-12-14 19:45:24 -0200

    Add interface for client rates

---
+++ interfaces/ClientRateInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: mauri870
+ * Date: 14/12/15
+ * Time: 19:28
+ */
+interface ClientRateInterface
+{
+    function setClientRate($rate);
+    function getClientRate();
+}
