5130271f17d0682387ae7e207c2d4005a81f9c52
Mauri de Souza Nunes <mauri870@gmail.com>
2015-12-14 19:54:21 -0200

    Add interface for custom adress

---
+++ interfaces/CustomBillingAdressInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: mauri870
+ * Date: 14/12/15
+ * Time: 19:47
+ */
+interface CustomBillingAdressInterface
+{
+    function setCustomBillingAdress($adress);
+    function getCustomBillingAdress();
+}
